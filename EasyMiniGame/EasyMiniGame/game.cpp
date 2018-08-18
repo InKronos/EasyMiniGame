3f7d4af97b9d5c1ead388e899cc6978f0e14920f
Bartłomiej Święch <warriorcjay@hotmail.com>
2018-08-18 23:16:31 +0200

    Game window

---
+++ EasyMiniGame/EasyMiniGame/game.cpp
@@ -0,0 +1,61 @@
+#include "game.h"
+
+Game::Game()
+{
+	std::cout << "Size and title not specified!" << std::endl;
+}
+
+Game::Game(const int & windowWidth, const int & windowHeight, const sf::String & windowTitle)
+	: window(sf::VideoMode(windowWidth, windowHeight), windowTitle)
+{
+	window.setFramerateLimit(30);
+}
+
+Game::~Game()
+{
+	std::cout << "You just deleted the window!" << std::endl;
+}
+
+void Game::run()
+{
+	while (this->window.isOpen())
+	{
+		events();
+		update();
+		draw();
+	}
+}
+
+void Game::events()
+{
+	sf::Event evnt;
+
+	while (this->window.pollEvent(evnt))
+	{
+		if (evnt.type == sf::Event::Closed)
+		{
+			this->window.close();
+			std::cout << "You just closed the window!" << std::endl;
+		}
+		if (evnt.type == sf::Event::Resized)
+		{
+			std::cout << "New width is " << this->window.getSize().x << ", new height is " << this->window.getSize().y << std::endl;
+		}
+	}
+
+}
+
+void Game::update()
+{
+}
+
+
+void Game::draw()
+{
+	this->window.clear();
+
+	this->window.display();
+
+}
+
+
